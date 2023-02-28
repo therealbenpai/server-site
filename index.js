2c56aa6c7246447d9dbb4aae44b7f36fe958218b
Benjamin Marsh <bmarsh@lisd.us>
2023-02-28 13:42:39 -0500

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express');
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
+const { ProfilingIntegration } = require('@sentry/profiling-node')
+const Intigrations = require('@sentry/integrations')
 const app = express();
 const port = 80;
 
 const website = require('./routes/index')
 const api = require('./routes/api')
 
-app.use('/', website);
+Sentry.init({
+    dsn: "https://82f73b485cdc419699fe0e52e2daadd0@o1236511.ingest.sentry.io/4504203537350656",
+    sampleRate: 1.0,
+    serverName: "Main PC",
+    integrations: [
+        new ProfilingIntegration(),
+        new Intigrations.ExtraErrorData({ depth: 10 }),
+        new Intigrations.SessionTiming(),
+        new Intigrations.Transaction(),
+        new Intigrations.ReportingObserver(),
+        new Intigrations.CaptureConsole({
+            levels: ['error', 'critical', 'fatal', 'warn']
+        }),
+        new Sentry.Integrations.Http({
+            tracing: true,
+            breadcrumbs: true
+        }),
+        new Tracing.Integrations.Express({ app })
+    ],
+    // @ts-ignore
+    profilesSampleRate: 1.0,
+    environment: "MasterDevelopment",
+    release: "v1.1.2-Private_Alpha",
+    sendDefaultPii: true
+});
+
+app.use(Sentry.Handlers.requestHandler({transaction: true}));
+app.use(Sentry.Handlers.tracingHandler());
 app.use('/api', api);
+app.use('/', website);
+app.use(Sentry.Handlers.errorHandler({shouldHandleError: () => { return true }}));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,35 @@
-// create an express router
-const express = require('express');
+const express = require('express')
 const router = express.Router();
 
+const util = require('../src/util-fuctions')
+const ccf = require('../src/crypto')
 
+router.use(express.json())
 
-module.exports = router;
+router.get('/favicon.ico', (_, res) => {
+    res.redirect('https://raw.githubusercontent.com/sparty182020/imagehost/main/favicon.ico')
+})
+
+router.get('/', (_, res) => {
+    res.sendFile('index.html', { root: `${process.cwd()}/api/`, headers: { 'x-timestamp': Date.now(), 'x-sent': true } })
+})
+
+router.get('/enc', (req, res) => {
+    if (!req.body.data) {
+        res.status(400).send(util.failedStatus('Missing data\nData: ' + req.body)).end()
+        return
+    }
+    const enc = ccf.semiEnc(req.body.data)
+    res.status(200).send({ enc: enc })
+})
+
+router.get('/dec', (req, res) => {
+    if (!req.body.data) {
+        res.status(400).send(util.failedStatus('Missing data'))
+        return
+    }
+    const dec = ccf.semiDec(req.body.data)
+    res.status(200).send({ dec: dec })
+})
+
+module.exports = router
