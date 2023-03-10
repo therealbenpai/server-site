2fb4159c869642f7a7ed92888a144308cdc8d127
Benjamin Marsh <bmarsh@lisd.us>
2023-03-09 21:07:35 -0500

    trial 2

--- routes/cdn.js
+++ routes/cdn.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2');
 
-router.get('/transcript/:id', (req, res) => {
+const connection = mysql.createConnection({
+    host: 'sparty18.com',
+    user: 'benpai',
+    password: 'benpaiIsCool',
+    database: 'benpai'
+});
+
+router.get('/transcript/:id', async (req, res) => {
     const { id } = req.params;
-    res.send(fs.readFileSync(`/CDN/BenpaiBot/transcripts/${id}`, 'utf8'));
+    const sql = `SELECT transcript FROM tickets WHERE id = ${id}`;
+    await connection.promise().query(sql).then(([rows, fields]) => {
+        if (rows.length === 0) return
+        res.send(res.send(fs.readFileSync(`/CDN/BenpaiBot/transcripts/${rows[0].transcript}`, 'utf8')))
+    })
 })
 
 module.exports = router;
