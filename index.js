341ffe607de20188ec29c97ce298e14bca521bba
Benjamin <benpai@sparty18.com>
2023-04-04 12:39:48 +0000

    try one

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const Sentry = require('@sentry/node');
 const Tracing = require('@sentry/tracing');
-const { ProfilingIntegration } = require('@sentry/profiling-node')
 const Intigrations = require('@sentry/integrations')
-const https = require('https')
-const fs = require('fs')
+const http = require('http')
 const app = express();
 const router = require('./routes/router');
 
@@ -13,7 +11,6 @@ Sentry.init({
     sampleRate: 1.0,
     serverName: "Main PC",
     integrations: [
-        new ProfilingIntegration(),
         new Intigrations.ExtraErrorData({ depth: 10 }),
         new Intigrations.SessionTiming(),
         new Intigrations.Transaction(),
@@ -28,7 +25,6 @@ Sentry.init({
         new Tracing.Integrations.Express({ app })
     ],
     // @ts-ignore
-    profilesSampleRate: 1.0,
     environment: "MasterDevelopment",
     release: "v1.1.2-Private_Alpha",
     sendDefaultPii: true
@@ -39,75 +35,65 @@ app.use(Sentry.Handlers.tracingHandler());
 app.set('view engine', 'pug');
 app.use('/', router);
 app.use((err, req, res, next) => {
-        switch (err.status) {
-            case 401:
-            case 403:
-                res.status(err.status).render(
-                    `misc/401.pug`,
+    switch (err.status) {
+        case 401:
+        case 403:
+            res.status(err.status).render(
+                `misc/401.pug`,
+                {
+                    title: `401 - Unauthorized`,
+                    path: req.path,
+                    code: err.status
+                }
+            );
+            break;
+        case 404:
+            res.status(404).render(
+                `misc/404.pug`,
+                {
+                    title: '404 - Page Not Found',
+                    path: req.path
+                }
+            );
+            break;
+        case 405:
+            // find the allowed methods for the path
+            const { path } = req;
+            const allowedMethods = app._router.stack
+                .filter(r => r.route && r.route.path === path)
+                .map(r => r.route.stack[0].method.toUpperCase())
+                .join(', ');
+            const methodUsed = req.method.toUpperCase();
+            res.status(405).render(
+                `misc/405.pug`,
+                {
+                    title: '405 - Method Not Allowed',
+                    path,
+                    allowedMethods,
+                    methodUsed
+                }
+            );
+            break;
+        default:
+            const errorId = Sentry.captureException(err);
+            res
+                .status(501)
+                .setHeader('X-Error-ID', errorId)
+                .render(
+                    `misc/501.pug`,
                     {
-                        title: `401 - Unauthorized`,
-                        path: req.path,
-                        code: err.status
+                        title: `501 - Internal Server Error`,
+                        errorId
                     }
-                );
-                break;
-            case 404:
-                res.status(404).render(
-                    `misc/404.pug`,
-                    {
-                        title: '404 - Page Not Found',
-                        path: req.path
-                    }
-                );
-                break;
-            case 405:
-                // find the allowed methods for the path
-                const { path } = req;
-                const allowedMethods = app._router.stack
-                    .filter(r => r.route && r.route.path === path)
-                    .map(r => r.route.stack[0].method.toUpperCase())
-                    .join(', ');
-                const methodUsed = req.method.toUpperCase();
-                res.status(405).render(
-                    `misc/405.pug`,
-                    {
-                        title: '405 - Method Not Allowed',
-                        path,
-                        allowedMethods,
-                        methodUsed
-                    }
-                );
-                break;
-            default:
-                const errorId = Sentry.captureException(err);
-                res
-                    .status(501)
-                    .setHeader('X-Error-ID', errorId)
-                    .render(
-                        `misc/501.pug`,
-                        {
-                            title: `501 - Internal Server Error`,
-                            errorId
-                        }
-                    )
-                break;
-        }
+                )
+            break;
     }
+}
 );
 
-switch (process.platform) {
-    case 'win32':
-    case 'darwin':
-        https.createServer({
-            key: fs.readFileSync(`${process.cwd()}\\assets\\certs\\server.key`),
-            cert: fs.readFileSync(`${process.cwd()}\\assets\\certs\\server.cert`)
-        }, app).listen(443, () => {
-            console.log(`Example app listening at https://localhost:443`)
-        })
-        break;
-    default:
-        app.listen(3001, () => {
-            console.log(`Example app listening at http://localhost:3001`)
-        })
-        break;
-}
+
+http
+.createServer(app)
+.listen(443, () => {
+    console.log(`Example app listening at https://localhost:443`)
+})
