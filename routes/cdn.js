3f8c669fbb419a185f7541388e3871a0b7bd1c55
Benjamin <benpai@sparty18.com>
2023-05-08 01:21:38 +0000

    update

--- routes/cdn.js
+++ routes/cdn.js
@@ -3,20 +3,17 @@ const router = express.Router();
 const fs = require('fs');
 const mariadb = require('mariadb');
 
-const connection = mariadb.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'benpai',
-    password: 'BenpaiIsCool',
-    database: 'sanrio'
-});
-
 router.get('/transcript/:id', async (req, res) => {
-    const { id } = req.params;
-    const sql = `SELECT transcript FROM tickets WHERE id = ${id}`;
-    (await connection).query(sql, (err, rows) => {
-        if (rows.length === 0) return
-        res.send(fs.readFileSync(`${process.env.HOME}/cdn/tickets/${rows[0].transcript}.html`, 'utf8'))
+    await mariadb.createConnection({
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'benpai',
+        password: 'BenpaiIsCool',
+        database: 'sanrio'
+    }).then(connection => connection.query(`SELECT transcript FROM tickets WHERE id = ${req.params.id}`))
+    .then(results => {
+        if (results.length === 0) return
+        res.send(fs.readFileSync(`/home/benpai/cdn/tickets/${results[0].transcript}.html`, 'utf8'))
     })
 })
 
